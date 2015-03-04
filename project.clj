cddc4673247fe6d7d95313b9bcc135ce96a86285
Frederic Peschanski <frederic.peschanski@lip6.fr>
2015-03-04 23:18:20 +0100

    new leiningen project

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject latte "0.0.1"
+  :description "LaTTe : a Laboratory for Type Theory experiments"
+  :url "https://github.com/fredokun/LaTTe.git"
+  :license {:name "MIT Licence"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot latte.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
---
+++ test/latte/core_test.clj
@@ -0,0 +1,7 @@
+(ns latte.core-test
+  (:require [clojure.test :refer :all]
+            [latte.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
