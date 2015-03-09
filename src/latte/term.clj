70da5fc4b12cd025fd7217926cb2c913e6f47a96
fredokun <Frederic.Peschanski@lip6.fr>
2015-03-09 23:17:48 +0100

    declaration variables

--- src/latte/term.clj
+++ src/latte/term.clj
@@ -9,7 +9,8 @@
 ;;}
 
 (ns latte.term
-  (:require [latte.utils :refer [example]]))
+  (:require [latte.utils :refer [example]]
+            [latte.termparser :as parser :refer [parse]]))
 
 (def ^:dynamic *examples-enabled* true)
 
@@ -20,7 +21,9 @@
 ;;}
 
 (defprotocol Unparser
-  (unparse [this]))
+  "A protocol for Unparsing term representations
+  as clojure expressions."
+  (unparse [expr] "Produce a `parsable` clojure expression from `expr`."))
 
 ;; (def ^:dynamic *examples-enabled* true) ;; to activate in-line examples
             
@@ -49,17 +52,40 @@
 ;; ### Universes
 
 ;; A *universe* is a set of types. The basic types are in the universe
-;;  of level 0 and each universe of level $n$ sits in a universe of level $n+1$.
+;; of level 0 and each universe of level $n$ sits in a universe of level $n+1$.
 
 ;;}
 
 
 (defrecord Univ [^int level])
 
-(defn mk-univ [level]
+(defn mk-univ
+  "Make a universe of the given `level` (a natural number)."
+  [level]
   (->Univ level))
 
 (example (:level (mk-univ 2)) => 2)
+
+(parser/register-term-list-parser
+ 'univ (fn [expr & _]
+         (parser/parse-list-check-arity expr 1)
+         (let [level (second expr)]
+           (if (not (and (integer? level)
+                         (>= level 0)))
+             (throw (Exception. (str "Universe level must be positive, given: " level)))
+             (mk-univ level)))))
+
+(example (parse '(univ 2)) => (mk-univ 2))
+
+(example (try
+           (parse '(univ -1))
+           (catch Exception e (.getMessage e)))
+         => "Universe level must be positive, given: -1")
+
+(example (try
+           (parse '(univ 1 bad))
+           (catch Exception e (.getMessage e)))
+           => "Wrong arity: expect 1 argument for 'univ'-expressions, given: 2")
 
 (extend-type Univ
   Unparser
@@ -67,3 +93,68 @@
     `(univ ~(:level u))))
 
 (example (unparse (mk-univ 2)) => `(univ 2))
+
+;;{
+
+;; ### Variables
+
+;; The variable occurrences of a term range in four distinct sets.
+
+;;    - declaration variables referencing type declarations
+
+;;    - definition variables reference definitional equalities (i.e. definitions)
+
+;;    - abstraction variables bound by a lambda abstraction
+
+;;    - product variables bound by universal quantification
+
+;; In this section we introduce the declarations and definitions names, t
+;; the last two will be introduced after their corresponding binders.
+
+;;}
+
+;;{
+
+;; #### Declaration variables
+
+;;}
+
+(defrecord DeclVar [name])
+
+(defn mk-decl-var
+  "Make the `name` declaration variable."
+  [name]
+  (->DeclVar name))
+
+(example (:name (mk-decl-var 'my-var))
+         => 'my-var)
+
+(defn match-decl-var?
+  [expr decl-env & _ ]
+  (and (symbol? expr)
+       (contains? decl-env expr)))
+
+(parser/register-term-other-parser
+ match-decl-var?
+ (fn [expr & _]
+   (mk-decl-var expr)))
+
+(example (parse 'my-var {'my-var (mk-univ 0)} {} ())
+         => (mk-decl-var 'my-var))
+
+(extend-type DeclVar
+  Unparser
+  (unparse [var]
+    (:name var)))
+
+(example (unparse (mk-decl-var 'my-var)) => 'my-var)
+
+;;{
+
+;; #### Definition variables
+
+;;}
+
+;; TODO
+
+
--- src/latte/termparser.clj
+++ src/latte/termparser.clj
@@ -22,19 +22,39 @@
 
 (defn register-term-other-parser
   "Register the parser function `pfun` for
-  (non-list) terms."
-  [pfun]
-  (swap! *term-other-parsers* (fn [parsers] (conj parsers pfun))))
+  (non-list) terms recognized by `pmatch?`."
+  [pmatch? pfun]
+  (swap! *term-other-parsers* (fn [parsers] (conj parsers [pmatch?, pfun]))))
 
-;; (defn parse-term
-;;   "Parse a term from the expression `expr`,
-;;   or signal a parsing problem."
-;;   [expr]
-;;   (if (list? expr)
-;;     (if (empty? expr)
-;;       (throw (Exception. "Parse error: empty-list"))
-;;       (let [parser (get (fetch-term-list-parsers) (car expr) :not-found)]
-;;         (if (= parser :not-found)
-;;           (throw (Exception. (str "Parse error: don't know how to parse '" (car expr) "'")))
-;;           (parser expr))))
-;;     (doseq [parser ;; TODO
+(defn- parse-list-term [expr decl-env def-env bind-env]
+  (if (empty? expr)
+    (throw (Exception. "Parse error: empty-list"))
+    (let [parser (get (fetch-term-list-parsers) (first expr) :not-found)]
+      (if (= parser :not-found)
+        (throw (Exception. (str "Parse error: don't know how to parse '" (first expr) "'-expressions")))
+        (parser expr decl-env def-env bind-env)))))
+
+(defn- parse-other-term [expr parsers decl-env def-env bind-env]
+  (if (seq parsers)
+    (let [[match?, parser] (first parsers)]
+      (if (match? expr decl-env def-env bind-env)
+        (parser expr decl-env def-env bind-env)
+        (recur expr (rest parsers) decl-env def-env bind-env)))
+    (throw (Exception. (str "Parse error: don't know how to parse '" expr "'")))))
+
+(defn parse-list-check-arity
+  "Check `arity` of `expr` list-term."
+  [expr arity]
+  (if (not= (count expr) (+ arity 1))
+    (throw (Exception. (str "Wrong arity: expect " arity " argument" (if (> arity 1) "s" "") " for '" (first expr) "'-expressions, given: " (- (count expr) 1))))
+    true))
+
+(defn parse
+  "Parse a term from the expression `expr`,
+  or signal a parsing problem."
+  ([expr] (parse expr {} {} ()))
+  ([expr decl-env def-env bind-env]
+   (if (list? expr)
+     (parse-list-term expr decl-env def-env bind-env)
+     (parse-other-term expr (deref *term-other-parsers*) decl-env def-env bind-env))))
+
