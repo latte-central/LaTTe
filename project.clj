8e7c639a706180d934213991cd41417bec67d2bf
Fredokun <fredokun@github.com>
2016-07-05 16:16:04 +0200

    better doc and version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject latte "0.0.2"
+(defproject latte "0.0.3"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-by-example "0.1.0-SNAPSHOT"]]
+  :codox {:metadata {:doc/format :markdown}}
   :plugins [[lein-codox "0.9.5"]])
 
