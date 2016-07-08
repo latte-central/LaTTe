560f1cc46969a727cccbbeef41783ed3f49f4bbb
Frederic Peschanski <not provided>
2016-07-08 18:13:27 +0200

    better (co)dox and version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject latte "0.0.3"
+(defproject latte "0.0.4"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-by-example "0.1.0-SNAPSHOT"]]
-  :codox {:metadata {:doc/format :markdown}}
+  :codox {:metadata {:doc/format :markdown}
+          :namespaces [latte.core latte.prop latte.classic
+                       latte.quant latte.equal]}
   :plugins [[lein-codox "0.9.5"]])
 
