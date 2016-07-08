afecce89d68625c2199d3578b9799f05645a002a
Frederic Peschanski <not provided>
2016-07-08 17:53:33 +0200

    substitutivity and congruence for equality

--- src/latte/equal.clj
+++ src/latte/equal.clj
@@ -57,6 +57,38 @@ This corresponds to Leibniz's *indiscernibility of identicals*."
     (have d _ :discharge [P (c)])
     (qed d)))
 
+(defthm eq-subst
+  "Substitutivity property of equality."
+  [[T :type] [P (==> T :type)] [x T] [y T]]
+  (==> (equal T x y)
+       (P x)
+       (P y)))
 
+(proof eq-subst :script
+  (assume [H1 (equal T x y)
+           H2 (P x)]
+    (have a (<=> (P x) (P y)) :by (H1 P))
+    (have b (P y) :by ((p/iff-elim-if (P x) (P y)) (a) H2))
+    (qed (b))))
 
-            
+(defthm eq-cong
+  "Congruence property of equality."
+  [[T :type] [U :type] [f (==> T U)] [x T] [y T]]
+  (==> (equal T x y)
+       (equal U (f x) (f y))))
+
+(proof eq-cong :script
+  (assume [H1 (equal T x y)
+           Q (==> U :type)]
+    (assume [H2 (Q (f x))]
+      (have a1 _ :by (eq-subst T (lambda [z T] (Q (f z))) x y))
+      (have a2 (Q (f y)) :by ((a1) H1 H2))
+      (have a (==> (Q (f x)) (Q (f y))) :discharge [H2 (a2)]))
+    (have b (equal T y x) :by ((eq-sym T x y) H1))
+    (assume [H3 (Q (f y))]
+      (have c1 _ :by (eq-subst T (lambda [z T] (Q (f z))) y x))
+      (have c2 (Q (f x)) :by ((c1) (b) H3))
+      (have c (==> (Q (f y)) (Q (f x))) :discharge [H3 (c2)]))
+    (have d (<=> (Q (f x)) (Q (f y))) :by ((p/iff-intro (Q (f x)) (Q (f y))) (a) (c)))
+    (have e (equal U (f x) (f y)) :discharge [Q (d)])
+    (qed (e))))
