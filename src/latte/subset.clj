5b8a12fd08ac2e982a4f67b645c02090cf2ac342
Fredokun <fredokun@github.com>
2016-07-12 17:30:10 +0200

    ordering and equality properties

--- src/latte/subset.clj
+++ src/latte/subset.clj
@@ -18,11 +18,11 @@
   such as dependent pairs and sigma-types."
 
   (:refer-clojure :exclude [and or not set])
-  
+
   (:require [latte.core :as latte :refer [defterm defthm forall assume have proof lambda]])
 
   (:require [latte.prop :as p :refer [<=> and or not]])
-  
+
   (:require [latte.equal :as eq :refer [equal]])
   )
 
@@ -43,11 +43,101 @@
     (==> (elem T x s1)
          (elem T x s2))))
 
+(defthm subseteq-refl
+  "The subset relation is reflexive."
+  [[T :type] [s (set T)]]
+  (subseteq T s s))
+
+(proof subseteq-refl :script
+  (assume [x T
+           H (elem T x s)]
+    (have a (elem T x s) :by H)
+    (have b (==> (elem T x s)
+                 (elem T x s)) :discharge [H (a)])
+    (have c (subseteq T s s) :discharge [x (b)])
+    (qed c)))
+
+(defthm subseteq-trans
+  "The subset relation is transitive."
+  [[T :type] [s1 (set T)] [s2 (set T)] [s3 (set T)]]
+  (==> (subseteq T s1 s2)
+       (subseteq T s2 s3)
+       (subseteq T s1 s3)))
+
+(proof subseteq-trans :script
+  (assume [H1 (subseteq T s1 s2)
+           H2 (subseteq T s2 s3)]
+    (assume [x T]
+      (have a (==> (elem T x s1)
+                   (elem T x s2)) :by (H1 x))
+      (have b (==> (elem T x s2)
+                   (elem T x s3)) :by (H2 x))
+      (have c (==> (elem T x s1)
+                   (elem T x s3)) :by ((p/impl-trans (elem T x s1)
+                                                     (elem T x s2)
+                                                     (elem T x s3)) (a) (b)))
+      (have d (subseteq T s1 s3) :discharge [x (c)]))
+    (qed d)))
+
 (defterm seteq
   "Equality on sets."
   [[T :type] [s1 (set T)] [s2 (set T)]]
   (and (subseteq T s1 s2)
        (subseteq T s2 s1)))
+
+(defthm seteq-refl
+  "Set equality is reflexive."
+  [[T :type] [s (set T)]]
+  (seteq T s s))
+
+(proof seteq-refl :script
+  (have a (subseteq T s s) :by (subseteq-refl T s))
+  (have b (and (subseteq T s s)
+               (subseteq T s s))
+        :by ((p/and-intro (subseteq T s s)
+                          (subseteq T s s)) (a) (a)))
+  (qed b))
+
+(defthm seteq-sym
+  "Set equality is symmetric."
+  [[T :type] [s1 (set T)] [s2 (set T)]]
+  (==> (seteq T s1 s2)
+       (seteq T s2 s1)))
+
+(proof seteq-sym :script
+  (assume [H (seteq T s1 s2)]
+    (have a (subseteq T s1 s2) :by ((p/and-elim-left (subseteq T s1 s2)
+                                                       (subseteq T s2 s1)) H))
+    (have b (subseteq T s2 s1) :by ((p/and-elim-right (subseteq T s1 s2)
+                                                        (subseteq T s2 s1)) H))
+    (have c (seteq T s2 s1) :by ((p/and-intro (subseteq T s2 s1)
+                                              (subseteq T s1 s2)) (b) (a)))
+    (qed c)))
+
+(defthm seteq-trans
+  "Set equality is transitive."
+  [[T :type] [s1 (set T)] [s2 (set T)] [s3 (set T)]]
+  (==> (seteq T s1 s2)
+       (seteq T s2 s3)
+       (seteq T s1 s3)))
+
+(proof seteq-trans :script
+  (assume [H1 (seteq T s1 s2)
+           H2 (seteq T s2 s3)]
+    (have a1 (subseteq T s1 s2) :by ((p/and-elim-left (subseteq T s1 s2)
+                                                      (subseteq T s2 s1)) H1))
+    (have b1 (subseteq T s2 s3) :by ((p/and-elim-left (subseteq T s2 s3)
+                                                      (subseteq T s3 s2)) H2))
+    (have c1 (subseteq T s1 s3) :by ((subseteq-trans T s1 s2 s3) (a1) (b1)))
+    (have a2 (subseteq T s2 s1) :by ((p/and-elim-right (subseteq T s1 s2)
+                                                       (subseteq T s2 s1)) H1))
+    (have b2 (subseteq T s3 s2) :by ((p/and-elim-right (subseteq T s2 s3)
+                                                       (subseteq T s3 s2)) H2))
+    (have c2 (subseteq T s3 s1) :by ((subseteq-trans T s3 s2 s1) (b2) (a2)))
+    (have d (seteq T s1 s3) :by ((p/and-intro (subseteq T s1 s3)
+                                              (subseteq T s3 s1)) (c1) (c2)))
+    (qed d)))
+
 
 (defterm union
   "Set union."
