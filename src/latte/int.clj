443a4c1cc2bfb37421be1c5a5c9a7aed721e3cac
Frederic Peschanski <not provided>
2016-07-25 16:06:19 +0200

    unique successor ... qed

--- src/latte/int.clj
+++ src/latte/int.clj
@@ -43,28 +43,6 @@
         :by ((rel/bijective-is-surjective int int succ) (succ-bijective)))
   (qed ((a) y)))
 
-(defthm single-succ-
-  "An integer `y` is the successor *at most* another integer."
-  [[y int]]
-  (forall [x1 x2 int]
-    (==> (equal int ((succ) x1) y)
-         (equal int ((succ) x2) y)
-         (equal int ((succ) x1) ((succ) x2)))))
-;; there is a problem with:
-;; (q/single int (lambda [x int] (equal int ((succ) x) y))))
-
-(proof single-succ- :script
-  (assume [x1 int
-           x2 int
-           u1 (equal int ((succ) x1) y)
-           u2 (equal int ((succ) x2) y)]
-    (have a (equal int y ((succ) x2)) :by ((eq/eq-sym int ((succ) x2) y) u2))
-    (have b (equal int ((succ) x1) ((succ) x2))
-          :by ((eq/eq-trans int ((succ) x1) y ((succ) x2))
-               u1 (a)))
-    (qed b)))
-
-
 (defthm single-succ
   "An integer `y` is the successor *at most* another integer."
   [[y int]]
@@ -79,67 +57,22 @@
     (have b (equal int ((succ) x1) ((succ) x2))
           :by ((eq/eq-trans int ((succ) x1) y ((succ) x2))
                H1 (a)))
-    (have c (==> ((lambda [x int] (equal int ((succ) x) y)) x2)
-                 (equal int ((succ) x1) ((succ) x2))) :discharge [H2 (b)])
-    (have d (==> ((lambda [x int] (equal int ((succ) x) y)) x1)
-                 ((lambda [x int] (equal int ((succ) x) y)) x2)
-                 (equal int ((succ) x1) ((succ) x2))) :discharge [H1 (c)])
-    (have e _ :discharge [x2 (d)])
-    (have f _ ;(q/single int (lambda [x int] (equal int ((succ) x) y)))
-          :discharge [x1 (e)]))
-  (qed f))
-
-
-(latte.kernel.norm/beta-delta-eq? {}
- (latte/term
-  [y int]
-  (forall [x1 x2 int]
-    (==> (equal int ((succ) x1) y)
-         (equal int ((succ) x2) y)
-         (equal int ((succ) x1) ((succ) x2)))))
- (latte/term
-  [y int]
-  (q/single int (lambda [x int] (equal int ((succ) x) y)))))
-
-(latte.kernel.norm/beta-delta-eq?
- {}
- (latte/term
-  [y int]
-  (forall [x1 x2 int]
-    (==> (equal int ((succ) x1) y)
-         (equal int ((succ) x2) y)
-         (equal int ((succ) x1) ((succ) x2)))))
- (latte/term
-  [y int]
-  (forall [x1 x2 int]
-    (==> ((lambda [x int] (equal int ((succ) x) y)) x1)
-         ((lambda [x int] (equal int ((succ) x) y)) x2)
-         (equal int ((succ) x1) ((succ) x2))))))
-
-;; this is probably a bug ...
-(latte.kernel.norm/beta-delta-eq?
- {}
- (latte/term
-  [y int]
-  (forall [x1 x2 int]
-    (==> ((lambda [x int] (equal int ((succ) x) y)) x1)
-         ((lambda [x int] (equal int ((succ) x) y)) x2)
-         (equal int ((succ) x1) ((succ) x2)))))
- (latte/term
-  [y int]
-  (q/single int (lambda [x int] (equal int ((succ) x) y)))))
-
-
-;; (definition single
-;;   "The constraint that \"there exists at most\"..."
-;;   [[T :type] [P (==> T :type)]]
-;;   (forall [x y T]
-;;     (==> (P x)
-;;          (P y)
-;;          (equal T x y))))
-
+    (have c (rel/injective int int succ)
+                                        ;(forall [x y int]
+                                        ;  (==> (equal int ((succ) x) ((succ y)))
+                                        ;       (equal int x y)))
+          :by ((rel/bijective-is-injective int int succ) (succ-bijective)))
+    (have d (equal int x1 x2) :by ((c) x1 x2 (b)))
+    (qed d)))
 
 (defthm unique-succ
   "There is a unique successor to an integer `y`."
-  [y int]
-  (q/unique int (lambda [x int] (equal int ((succ) x) y)))) 
+  [[y int]]
+  (q/unique int (lambda [x int] (equal int ((succ) x) y))))
+
+(proof unique-succ :script
+  (qed ((p/and-intro (q/ex int (lambda [x int] (equal int ((succ) x) y)))
+                     (q/single int (lambda [x int] (equal int ((succ) x) y))))
+        (exist-succ y)
+        (single-succ y))))
+
