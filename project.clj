5bef2c73586539a7731ac24559ab93667e978a84
Fredokun <fredokun@github.com>
2016-08-02 11:51:58 +0200

    removing ints from lib

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject latte "0.0.6"
+(defproject latte "0.1.0-SNAPSHOT"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-by-example "0.1.0-SNAPSHOT"]]
+                 [clj-by-example "0.1.0"]]
   :codox {:metadata {:doc/format :markdown}
           :namespaces [latte.core latte.prop latte.classic
-                       latte.quant latte.equal latte.subset latte.rel latte.int]}
+                       latte.quant latte.equal latte.subset latte.rel]}
   :plugins [[lein-codox "0.9.5"]])
 
