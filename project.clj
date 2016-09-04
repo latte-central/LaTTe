882596cba5abe19c83721e3179f772b024bcdc74
Frederic Peschanski <not provided>
2016-09-04 19:27:10 +0200

    patch version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject latte "0.3.1-SNAPSHOT"
+(defproject latte "0.3.2-SNAPSHOT"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
