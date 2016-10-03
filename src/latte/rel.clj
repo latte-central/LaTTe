153e4651fea2c749831bd7396745019b122948b7
Fredokun <fredokun@github.com>
2016-10-03 15:10:27 +0200

    composition of injections

--- src/latte/rel.clj
+++ src/latte/rel.clj
@@ -14,7 +14,7 @@ This namespace provides some important properties about such
   (:refer-clojure :exclude [and or not])
 
   (:require [latte.core :as latte :refer [definition defaxiom defthm
-                                          forall ==>
+                                          forall lambda ==>
                                           proof assume have]]
             [latte.prop :as p :refer [and or not]]
             [latte.equal :as eq :refer [equal]]
@@ -91,4 +91,37 @@ This namespace provides some important properties about such
   (assume [H (bijective T U F)]
     (have a (injective T U F) :by (p/%and-elim-left H))
     (qed a)))
+
+
+(definition compose
+  "The composition of two functions."
+  [[T :type] [U :type] [V :type]
+   [f (==> U V)] [g [==> T U]]]
+  (lambda [x T] (f (g x))))
+
+(defthm compose-injective
+  "The composition of two injective functions is injective."
+  [[T :type] [U :type] [V :type]
+   [f (==> U V)] [g [==> T U]]]
+  (==> (injective U V f)
+       (injective T U g)
+       (injective T V (compose T U V f g))))
+
+(proof compose-injective
+    :script
+  (assume [Hf (injective U V f)
+           Hg (injective T U g)]
+    (assume [x T
+             y T
+             Hinj (equal V
+                         ((compose T U V f g) x)
+                         ((compose T U V f g) y))]
+      (have <a> (equal V (f (g x)) (f (g y))) :by Hinj)
+      (have <b> (equal U (g x) (g y))
+            :by (Hf (g x) (g y) <a>))
+      (have <c> (equal T x y)
+            :by (Hg x y <b>))
+      (have <d> (injective T V (compose T U V f g))
+            :discharge [x y Hinj <c>]))
+    (qed <d>)))
 
