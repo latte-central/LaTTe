237511a69ed74ee68ab772f43352d077c183109b
Fredokun <fredokun@github.com>
2017-01-18 11:25:46 +0100

    parser rewritten (exists parsing should be through notations)

--- src/latte/kernel/presyntax.clj
+++ src/latte/kernel/presyntax.clj
@@ -1,7 +1,8 @@
 
 (ns latte.kernel.presyntax
   (:require [clj-by.example :refer [example do-for-example]]
-            [latte.kernel.defenv :as defenv]))
+            [latte.kernel.defenv :as defenv]
+            [latte.kernel.syntax :as stx]))
 
 (def ^:private +examples-enabled+)
 
@@ -25,13 +26,15 @@
          parse-symbol-term)
 
 (defn parse-term
-  ([def-env t] (parse-term def-env t #{}))
-  ([def-env t bound]
+  "Parse a presyntax term `t` in environment `def-env` (for checking references)
+at the given `level` (number of binders above) with binders `bmap` (mapping a binder name to a level `k`)."
+  ([def-env t] (parse-term def-env t 0 {}))
+  ([def-env t level bmap]
    (cond
      (kind? t) [:ok '□]
      (type? t) [:ok '✳]
-     (sequential? t) (parse-compound-term def-env t bound)
-     (symbol? t) (parse-symbol-term def-env t bound)
+     (sequential? t) (parse-compound-term def-env t level bmap)
+     (symbol? t) (parse-symbol-term def-env t level bmap)
      :else [:ko {:msg "Cannot parse term" :term t}])))
 
 (example
@@ -40,29 +43,26 @@
 (example
  (parse-term {} :type) => '[:ok ✳])
 
-(defn parse-symbol-term [def-env sym bound]
+(defn parse-symbol-term [def-env sym level bmap]
   ;;(println "[parse-symbol-term] sym=" sym)
-  (cond
-    (reserved-symbol? sym) [:ko {:msg "Symbol is reserved" :term sym}]
-    (contains? bound sym) [:ok sym]
-    (defenv/registered-definition? def-env sym) [:ok (list (defenv/qualify-def def-env sym))]
-    :else
-    ;; free variable
-    [:ok sym]))
+  (if (reserved-symbol? sym)
+    [:ko {:msg "Symbol is reserved" :term sym}]
+    (if-let [sym-level (get bmap sym)]
+      [:ok (stx/mk-bvar (- level (inc sym-level)))]
+      (if (defenv/registered-definition? def-env sym)
+        [:ok (stx/mk-ref (defenv/qualify-def def-env sym) [])]
+        ;; else it's a free variable
+        [:ok (stx/mk-fvar sym)]))))
 
 (example
- (parse-term {} 'x #{'x}) => '[:ok x])
+ (parse-term {} 'x 3 {'x 1}) => [:ok (stx/mk-bvar 1)])
 
 (example
- (parse-term {} 'x #{'y}) => '[:ok x])
+ (parse-term {} 'y 3 {'x 1}) => [:ok (stx/mk-fvar 'y)])
 
 (example
  (parse-term {'x {:arity 0}} 'x)
- => '[:ok (x)])
-
-(example
- (parse-term {'x {:arity 1}} 'x)
- => '[:ok (x)])
+ => [:ok (stx/mk-ref 'x [])])
 
 (defn lambda-kw? [t]
   (= t 'λ))
@@ -74,25 +74,20 @@
 (defn arrow-kw? [t]
   (= t '⟶))
 
-(defn exists-kw? [t]
-  (= t '∃))
-
 (declare parse-lambda-term
          parse-product-term
          parse-arrow-term
-         parse-exists-term
          parse-defined-term
          parse-application-term)
 
-(defn parse-compound-term [def-env t bound]
+(defn parse-compound-term [def-env t level bmap]
   ;; (println "[parse-compound-term] t=" t)
   (if (empty? t)
     [:ko {:msg "Compound term is empty" :term t}]
     (cond
-      (lambda-kw? (first t)) (parse-lambda-term def-env t bound)
-      (product-kw? (first t)) (parse-product-term def-env t bound)
-      (arrow-kw? (first t)) (parse-arrow-term def-env t bound)
-      (exists-kw? (first t)) (parse-exists-term def-env t bound)
+      (lambda-kw? (first t)) (parse-lambda-term def-env t level bmap)
+      (product-kw? (first t)) (parse-product-term def-env t level bmap)
+      (arrow-kw? (first t)) (parse-arrow-term def-env t level bmap)
       :else
       (if (and (or (symbol? (first t)) (var? (first t)))
                (defenv/registered-definition? def-env (first t)))
@@ -105,13 +100,13 @@
                             {:def sdef}))
             (and (= (:arity sdef) 0)
                  (seq (rest t)))
-            (parse-application-term def-env (cons (list (first t)) (rest t)) bound)
+            (parse-application-term def-env (cons (list (first t)) (rest t)) level bmap)
             :else
-            (parse-defined-term def-env sdef t bound)))
+            (parse-defined-term def-env sdef t level bmap)))
         ;; else
-        (parse-application-term def-env t bound)))))
+        (parse-application-term def-env t level bmap)))))
 
-(defn parse-binding [def-env v bound]
+(defn parse-binding [def-env v level bmap]
   (cond
     (not (vector? v))
     [:ko {:msg "Binding is not a vector" :term v}]
@@ -119,7 +114,7 @@
     [:ko {:msg "Binding must have at least 2 elements" :term v}]
     :else
     (let [ty (last v)
-          [status ty'] (parse-term def-env ty bound)]
+          [status ty'] (parse-term def-env ty level bmap)]
       (if (= status :ko)
         [:ko {:msg "Wrong binding type" :term v :from ty'}]
         (loop [s (butlast v), vars #{}, res []]
@@ -135,60 +130,83 @@
             [:ok res]))))))
 
 (example
- (parse-binding {} '[x :type] #{})
+ (parse-binding {} '[x :type] 0 {})
  => '[:ok [[x ✳]]])
 
 (example
- (parse-binding {} '[x y z :type] #{})
+ (parse-binding {} '[x y z :type] 0 {})
  => '[:ok [[x ✳] [y ✳] [z ✳]]])
 
 (example
- (parse-binding {} '[x y Π :type] #{})
+ (parse-binding {} '[x y Π :type] 0 {})
  => '[:ko {:msg "Wrong binding variable: symbol is reserved",
            :term [x y Π :type],
            :symbol Π}])
 
 (example
- (parse-binding {} '[x y x :type] #{})
+ (parse-binding {} '[x y x :type] 0 {})
  => '[:ko {:msg "Duplicate binding variable", :term [x y x :type], :var x}])
 
 (example
- (parse-binding {} '[x] #{})
+ (parse-binding {} '[x] 0 {})
  => '[:ko {:msg "Binding must have at least 2 elements", :term [x]}])
 
 (example
- (parse-binding {} '[x y :bad] #{})
+ (parse-binding {} '[x y :bad] 0 {})
  => '[:ko {:msg "Wrong binding type", :term [x y :bad], :from {:msg "Cannot parse term", :term :bad}}])
 
-(defn parse-binder-term [def-env binder t bound]
+(defn parse-binder-term [def-env binder t level bmap]
   (if (< (count t) 3)
     [:ko {:msg (str "Wrong " binder " form (expecting at least 3 arguments)") :term t :nb-args (count t)}]
-    (let [[status bindings] (parse-binding def-env (second t) bound)]
+    (let [[status bindings] (parse-binding def-env (second t) level bmap)]
       (if (= status :ko)
         [:ko {:msg (str "Wrong bindings in " binder " form") :term t :from bindings}]
-        (let [bound' (reduce (fn [res [x _]]
-                               (conj res x)) #{} bindings)]
+        (let [[bmap' level'] (loop [bindings bindings, level level, bmap bmap]
+                               (if (seq bindings)
+                                 (recur (rest bindings) (inc level) (assoc bmap (ffirst bindings) level))
+                                 [bmap level]))]
           (let [body (if (= (count t) 3)
                        (nth t 2)
                        (rest (rest t)))]
-            (let [[status body] (parse-term def-env body bound')]
+            (let [[status body] (parse-term def-env body level' bmap')]
               (if (= status :ko)
                 [:ko {:msg (str "Wrong body in " binder " form") :term t :from body}]
-                (loop [i (dec (count bindings)), res body]
-                  (if (>= i 0)
-                    (recur (dec i) (list binder (bindings i) res))
-                    [:ok res]))))))))))
+                (let [binder-fun (cond
+                                   (lambda-kw? binder) stx/mk-lambda
+                                   (product-kw? binder) stx/mk-prod
+                                   :else (throw (ex-info "Not a known binder (please report)" {:term t, :binder binder})))]
+                  (loop [i (dec (count bindings)), res body]
+                    (if (>= i 0)
+                      (let [[x ty] (bindings i)]
+                        (recur (dec i) (binder-fun x ty res)))
+                      [:ok res])))))))))))
 
-(defn parse-lambda-term [def-env t bound]
-  (parse-binder-term def-env 'λ t bound))
+(defn parse-lambda-term [def-env t level bmap]
+  (parse-binder-term def-env 'λ t level bmap))
 
 (example
- (parse-term {} '(λ [x :type] x))
-  => '[:ok (λ [x ✳] x)])
+ (stx/unparse-ln (second (parse-term {} '(λ [x :type] x) 0 {})))
+ => '(λ [✳] :0))
 
 (example
- (parse-term {} '(λ [x y :type] x))
- => '[:ok (λ [x ✳] (λ [y ✳] x))])
+ (stx/unparse-ln (second (parse-term {} '(λ [x :type] (λ [y :type] y)))))
+ => '(λ [✳] (λ [✳] :0)))
+
+(example
+ (stx/unparse-ln (second (parse-term {} '(λ [x :type] (λ [y :type] x)))))
+ => '(λ [✳] (λ [✳] :1)))
+
+(example
+ (stx/unparse-ln (second (parse-term {} '(λ [x :type] (λ [y :type] z)))))
+ => '(λ [✳] (λ [✳] z)))
+
+(example
+ (stx/unparse-ln (second (parse-term {} '(λ [x y :type] x))))
+ => '(λ [✳] (λ [✳] :1)))
+
+(example
+ (stx/unparse-ln (second (parse-term {} '(λ [x y :type] y))))
+ => '(λ [✳] (λ [✳] :0)))
 
 (example
  (parse-term {} '(λ [x x :type] x))
@@ -202,87 +220,64 @@
            :term (λ [x] x),
            :from {:msg "Binding must have at least 2 elements", :term [x]}}])
 
-(example
- (parse-term {} '(λ [x :type] z))
- => '[:ok (λ [x ✳] z)])
-
-(defn parse-product-term [def-env t bound]
-  (parse-binder-term def-env 'Π t bound))
+(defn parse-product-term [def-env t level bmap]
+  (parse-binder-term def-env 'Π t level bmap))
 
 (example
- (parse-term {} '(Π [x :type] x))
- => '[:ok (Π [x ✳] x)])
+ (stx/unparse-ln (second (parse-term {} '(Π [x :type] x))))
+ => '(Π [✳] :0))
 
 (example
- (parse-term {} '(Π [x y :type] x))
- => '[:ok (Π [x ✳] (Π [y ✳] x))])
+ (stx/unparse-ln (second (parse-term {} '(Π [x y :type] x))))
+ => '(Π [✳] (Π [✳] :1)))
 
-(defn parse-terms [def-env ts bound]
+(defn parse-terms [def-env ts level bmap]
   (reduce (fn [res t]
-            (let [[status t' :as tres] (parse-term def-env t bound)]
+            (let [[status t' :as tres] (parse-term def-env t level bmap)]
               (if (= status :ok)
                 [:ok (conj (second res) t')]
                 (reduced tres)))) [:ok []] ts))
 
 (example
- (parse-terms {} '(x y z) #{'x 'y 'z})
- => '[:ok [x y z]])
+ (parse-terms {} '(x y z) 0 {})
+ => [:ok [(stx/mk-fvar 'x) (stx/mk-fvar 'y) (stx/mk-fvar 'z)]])
 
 (example
- (parse-terms {} '(x y z) #{'x 'z})
- => '[:ok [x y z]])
+ (parse-terms {} '(x y z) 4 '{x 1, z 2})
+ => [:ok [(stx/mk-bvar 2) (stx/mk-fvar 'y) (stx/mk-bvar 1)]])
 
-(defn parse-arrow-term [def-env t bound]
+(defn parse-arrow-term [def-env t level bmap]
   (if (< (count t) 3)
     [:ko {:msg "Arrow (implies) requires at least 2 arguments"
           :term t
           :nb-args (count (rest t))}]
-    (let [[status ts'] (parse-terms def-env (rest t) bound)]
-      (if (= status :ko)
-        [:ko {:msg "Cannot parse arrow." :term t :from ts'}]
-        (loop [ts (rest (reverse ts')), res (last ts')]
-          (if (seq ts)
-            (recur (rest ts) (list 'Π ['⇧ (first ts)] res))
-            [:ok res]))))))
+    (let [prod
+          (loop [ts (butlast (rest t)), res (last t)]
+            (if (seq ts)
+              (recur (rest ts) (list 'Π ['⇧ (first ts)] res))
+              res))]
+      (let [[status res] (parse-term def-env prod level bmap)]
+        (if (= status :ko)
+          [:ko {:msg "Cannot parse arrow term"
+                :term t
+                :from res}]
+          [:ok res])))))
 
 (example
- (parse-term {} '(⟶ :type :type))
- => '[:ok (Π [⇧ ✳] ✳)])
+ (stx/unparse-ln (second (parse-term {} '(⟶ :type :type))))
+ => '(Π [✳] ✳))
 
 (example
- (parse-term {} '(⟶ sigma tau mu))
- => '[:ok (Π [⇧ sigma] (Π [⇧ tau] mu))])
+ (stx/unparse-ln (second (parse-term {} '(⟶ sigma tau mu))))
+ => '(Π [tau] (Π [sigma] mu)))
 
-(defn parse-exists-term [def-env t bound]
-  (if (< (count t) 3)
-    [:ko {:msg (str "Wrong `exists` form (expecting at least 3 arguments)") :term t :nb-args (count t)}]
-    (let [[status bindings] (parse-binding def-env (second t) bound)]
-      (if (= status :ko)
-        [:ko {:msg (str "Wrong bindings in `exists` form") :term t :from bindings}]
-        (let [bound' (reduce (fn [res [x _]]
-                               (conj res x)) #{} bindings)]
-          (let [body (if (= (count t) 3)
-                       (nth t 2)
-                       (rest (rest t)))]
-            (let [[status body] (parse-term def-env body bound')]
-              (if (= status :ko)
-                [:ko {:msg (str "Wrong body in `exists` form") :term t :from body}]
-                (loop [i (dec (count bindings)), res body]
-                  (if (>= i 0)
-                    (recur (dec i) (list (resolve 'latte.quant/ex) (second (bindings i)) (list 'λ (bindings i) res)))
-                    [:ok res]))))))))))
-
-(example
- (parse-term {} '(∃ [x T] P))
- => [:ok (list (resolve 'latte.quant/ex) 'T '(λ [x T] P))])
-
-(defn parse-defined-term [def-env sdef t bound]
+(defn parse-defined-term [def-env sdef t level bmap]
   (if (defenv/notation? sdef)
     (let [notation-fn (defenv/get-notation-fn sdef)
           [status t'] (apply notation-fn (rest t))]
       (if (= status :ko)
         [:ko t']
-        (parse-term def-env t' bound)))
+        (parse-term def-env t' level bmap)))
     ;; other definitions
     (let [def-name (first t)
           arity (count (rest t))]
@@ -290,61 +285,46 @@
                (< (:arity sdef) arity))
         [:ko {:msg "Too many arguments for definition." :term t :def-name def-name :arity arity :nb-args (:arity sdef)}]
         ;; else
-        (let [[status ts] (parse-terms def-env (rest t) bound)]
+        (let [[status ts] (parse-terms def-env (rest t) level bmap)]
           (if (= status :ko)
             [:ko {:msg "Wrong argument" :term t :from ts}]
-            [:ok (list* (defenv/qualify-def def-env def-name) ts)]))))))
+            [:ok (stx/mk-ref (defenv/qualify-def def-env def-name) ts)]))))))
 
 (example
- (parse-term {'ex (defenv/map->Definition {:arity 2})}
-             '(ex x :kind) #{'x})
- => '[:ok (ex x □)])
+ (stx/unparse-ln (second (parse-term {'ex (defenv/map->Definition {:arity 3})}
+                                     '(ex x y :kind) 2 {'x 1})))
+ => '(ex :0 y □))
 
 (example
- (parse-term {'ex (defenv/map->Definition {:arity 3})}
-             '(ex x y z) '#{x y z})
- => '[:ok (ex x y z)])
+ (stx/unparse-ln (second (parse-term {'ex (defenv/map->Definition {:arity 3})}
+                                 '(ex x y z) 3 '{x 0 y 1 z 2})))
+ => '(ex :2 :1 :0))
 
-(defn left-binarize [t]
-  (if (< (count t) 2)
-    t
-    (loop [s (rest (rest t)), res [(first t) (second t)]]
-      (if (seq s)
-        (recur (rest s) [res (first s)])
-        res))))
-
-(example
- (left-binarize '(a b)) => '[a b])
-
-(example
- (left-binarize '(a b c)) => '[[a b] c])
-
-(example
- (left-binarize '(a b c d e)) => '[[[[a b] c] d] e])
-
-(defn parse-application-term [def-env t bound]
+(defn parse-application-term [def-env t level bmap]
   ;; (println "[parse-application-term] t=" t)
   (if (< (count t) 2)
     [:ko {:msg "Application needs at least 2 terms" :term t :nb-terms (count t)}]
-    (let [[status ts] (parse-terms def-env t bound)]
+    (let [[status ts] (parse-terms def-env t level bmap)]
       ;; (println "   ==> " (left-binarize ts))
       (if (= status :ko)
         [:ko {:msg "Parse error in operand of application" :term t :from ts}]
-        [:ok (left-binarize ts)]))))
+        [:ok (apply stx/mk-app ts)]))))
 
 (example
- (parse-term {} '(x y) '#{x y}) => '[:ok [x y]])
+ (stx/unparse-ln (second (parse-term {} '(x y) 3 '{x 1, y 2})))
+ => '(:1 :0))
 
 (example
- (parse-term {} '(x y z) '#{x y z}) => '[:ok [[x y] z]])
+ (stx/unparse-ln (second (parse-term {} '(x y z) 3 '{x 1, y 2})))
+ => '(:1 :0 z))
 
 (example
- (parse-term {} '(x y z t) '#{x y z t}) => '[:ok [[[x y] z] t]])
+ (stx/unparse-ln (second (parse-term {} '(t x y z) 3 '{x 1, y 2})))
+ => '(t :1 :0 z))
 
 (example
- (parse-term {} '(λ [x :type] x :type :kind))
- => '[:ok (λ [x ✳] [[x ✳] □])])
-
+ (stx/unparse-ln (second (parse-term {} '(λ [x :type] x :type :kind))))
+ => '(λ [✳] (:0 ✳ □)))
 
 (defn parse
   ([t] (parse {} t))
@@ -352,3 +332,4 @@
                  (if (= status :ko)
                    (throw (ex-info "Parse error" t'))
                    t'))))
+
