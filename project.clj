02c27f2d2de57748a0bbbc3b0c52b10515353008
Fredokun <fredokun@github.com>
2017-02-22 16:10:49 +0100

    new version on clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject latte "0.6.0-SNAPSHOT"
+(defproject latte "0.6.1-SNAPSHOT"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
