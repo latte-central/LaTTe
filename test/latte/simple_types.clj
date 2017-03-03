14f26d15006ce26802974366ecbe8c73e6470a8a
Bost <thebost@gmail.com>
2017-03-03 13:20:50 +0100

    Fix failing `lein test`

--- test/latte/simple_types.clj
+++ test/latte/simple_types.clj
@@ -1,6 +1,7 @@
 (ns latte.simple-types
   (:require [clojure.test :as test :refer [deftest is]])
-  (:require [latte.core :as l :refer [=== type-of term check-type?]])
+  (:require [latte.core :as l :refer [==> term lambda type-of type-check?]])
+  (:require [latte.kernel.norm :as n :refer [beta-eq?]])
   )
 
 ;;{
@@ -16,32 +17,28 @@
 ;;}
 
 (deftest basic-terms
-  (is (=== (type-of [sigma :type]
-                    (lambda [x sigma] x))
-           (term [sigma :type] (==> sigma sigma))))
+  ;; Examples 2.2.6
+  ;; (1) When x has type σ, then the identity function λx . x has type σ → σ
+  (is (beta-eq? (type-of [sigma :type] (lambda [x sigma] x)) ; (Π [x sigma] sigma)
+                (term [sigma :type] (==> sigma sigma))       ; (Π [⇧ sigma] sigma)
+                ))
 
-  (is (check-type? [sigma :type]
-                   (lambda [x sigma] x)
+  (is (type-check? [sigma :type] (lambda [x sigma] x)
                    (==> sigma sigma)))
-  
-  (is (=== (type-of [sigma :type]
-                    [tau :type]
-                    [y (--> sigma tau)]
-                    [x sigma]
-                    (y x))
-          'tau))
 
-  (is (=== (type-of [alpha :type] [beta :type] [gamma :type]
-                    [x (--> alpha alpha)]
-                    [y (--> (--> alpha alpha) beta)]
-                    (lambda [u gamma] (y x)))
-           (term [beta :type] [gamma :type]
-                 (--> gamma beta))))
+  (is (beta-eq? (type-of [sigma :type]
+                         [tau :type]
+                         [y (==> sigma tau)]
+                         [x sigma]
+                         (y x))      ; tau
+                'tau                 ; tau
+                ))
+
+  (is (beta-eq? (type-of [alpha :type] [beta :type] [gamma :type]
+                         [x (==> alpha alpha)]
+                         [y (==> (==> alpha alpha) beta)]
+                         (lambda [u gamma] (y x)))   ; (Π [u gamma] beta)
+                   (term [beta :type] [gamma :type]
+                         (==> gamma beta))           ; (Π [⇧ gamma] beta)
+                   ))
   )
-
-
-
-
-
-
-
