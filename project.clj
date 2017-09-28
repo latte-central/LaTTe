c07c3f110fab1109459437ca1c45b2f8559ba941
fredokun <frederic.peschanski@lip6.fr>
2017-09-28 11:11:30 +0200

    kernel dependency

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject latte "0.6.1-SNAPSHOT"
+(defproject latte "0.7.0-SNAPSHOT"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-by-example "0.1.0"]]
+                 [latte-kernel "0.99.2-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte.core latte.prop latte.classic
                        latte.quant latte.equal latte.rel latte.fun]}
-  :plugins [[lein-codox "0.10.1"]])
+  :plugins [[lein-codox "0.10.3"]])
 
