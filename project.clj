49550e398d2a3818466dbec51df4a4f40c5cf78f
fredokun <frederic.peschanski@lip6.fr>
2017-10-06 15:11:22 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject latte "0.99.3-SNAPSHOT"
+(defproject latte "0.99.4-SNAPSHOT"
   :description "LaTTe : a Laboratory for Type Theory Experiments"
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/core.match "0.3.0-alpha5"]
-                 [latte-kernel "0.99.3-SNAPSHOT"]]
+                 [latte-kernel "0.99.4-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte.core latte.prop latte.classic
