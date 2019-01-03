c50277d03261f1982156b99d8563d2c8c12a5507
fredokun <frederic.peschanski@lip6.fr>
2019-01-03 13:07:02 +0100

    b2 release

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/fredokun/LaTTe.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [digest "1.4.8"]
                  [com.taoensso/timbre "4.10.0"]
-                 [latte-kernel "1.0b1-SNAPSHOT"]]
+                 [latte-kernel "1.0b2-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte.core]}
