d049cc8c890e7afaa733c60ad23f5dd11633d32c
fredokun <frederic.peschanski@lip6.fr>
2022-11-24 15:52:02 +0100

    started match for opt, star and plus variables

--- src/latte/search.clj
+++ src/latte/search.clj
@@ -14,14 +14,100 @@
       (and (> (count sname) 1)
            (= (first sname) \?)))))
 
-(declare match-list
+(comment
+  (variable? '?)
+  (variable? '?X)
+  (variable? '?X*)
+  (variable? '?X+)
+)
+
+(defn simple-variable? [v]
+  (and (variable? v)
+       (not (#{\* \+ \?} (last (name v))))))
+
+(comment
+  (simple-variable? '?)
+  (simple-variable? '?X)
+  (simple-variable? '?X*)
+  (simple-variable? '?X+)
+)
+
+
+(defn star-variable? [v]
+  (and (variable? v)
+       (= (last (name v)) \*)))
+
+(comment
+  (star-variable? '?X)
+  (star-variable? '?)
+  (star-variable? '?*)
+  (star-variable? '?X*)
+  (star-variable? '?_*)
+)
+
+(defn ext-variable-name [sv]
+  (let [svname (name sv)]
+    (subs svname 1 (dec (count svname)))))
+
+(comment
+  (ext-variable-name '?*)
+  (ext-variable-name '?X*)
+  (ext-variable-name '?_*)
+)
+
+(defn ext-variable-sym [sv]
+  (symbol (str "?" (ext-variable-name sv))))
+
+(comment
+  (ext-variable-sym '?X*)
+  (ext-variable-sym '?*)
+)
+
+(defn opt-variable? [v]
+  (and (variable? v)
+       (= (last (name v)) \?)))
+
+(comment
+  (opt-variable? '?X)
+  (opt-variable? '?)
+  (opt-variable? '??)
+  (opt-variable? '?X?)
+  (opt-variable? '?_?)
+
+  (ext-variable-name '??)
+  (ext-variable-name '?X?)
+  (ext-variable-name '?_?)
+)
+
+(defn plus-variable? [v]
+  (and (variable? v)
+       (= (last (name v)) \+)))
+
+(comment
+  (plus-variable? '?X)
+  (plus-variable? '?)
+  (plus-variable? '?+)
+  (plus-variable? '?X+)
+  (plus-variable? '?_+)
+)
+
+(comment
+  (ext-variable-name '?+)
+  (ext-variable-name '?X+)
+  (ext-variable-name '?_+)
+)
+
+(defrecord Repeat [patt min max sol cont])
+
+(declare match-repeat
+         match-list
          match-var)
          
 (defn match 
   ([patt term] (match {} patt term))
   ([env patt term]
    (cond
-     (variable? patt) (match-var env patt term)
+     (simple-variable? patt) (match-var env patt term)
      (list? patt) (if-not (list? term)
                     false
                     (match-list env patt term))
@@ -38,20 +124,70 @@
     ;; no previous binding
     (assoc env var term)))
 
+(declare count-min-expected-terms
+         match-star)
+
 (defn match-list [env patts terms]
   (if (seq patts)
     (if (seq terms)
-      (let [patt (first patts)
-            term (first terms)]
-        (if-let [env' (match env patt term)]
-          (recur env' (rest patts) (rest terms))
-          false))
+      (let [min-count (count-min-expected-terms patts)]
+        (if (< (count terms) min-count)
+          false ; not enough terms in list
+          (let [patt (first patts)
+                term (first terms)]
+            (cond
+              (opt-variable? patt)
+              (if-let [env' (match env (ext-variable-sym patt) term)]
+                (recur env' (rest patts) (rest terms))
+                (recur env (rest patts) terms))
+
+              (star-variable? patt)
+              (do (when (get env (ext-variable-sym patt))
+                    (throw (ex-info "Multiple occurrence of zero-or-many variable" {:variable patt                                                                          :env env})))
+                  (let [[matches terms'] (match-star (ext-variable-sym patt) env terms)]
+                    (recur (assoc env (ext-variable-sym patt) matches) (rest patts) terms')))
+              
+              (plus-variable? patt)
+              (do (when (get env (ext-variable-sym patt))
+                    (throw (ex-info "Multiple occurrence of one-or-many variable" {:variable patt                                                                          :env env})))
+                  (let [[matches terms'] (match-star (ext-variable-sym patt) env terms)]
+                    (if (zero? (count matches))
+                      false ; not enough matches                     
+                      (recur (assoc env (ext-variable-sym patt) matches) (rest patts) terms'))))
+
+              :else ; other kind of pattern
+              (if-let [env' (match env patt term)]
+                (recur env' (rest patts) (rest terms))
+                false)))))
       ;; no element in term
       false)
     ;; nothing left to match
     (if (seq terms)
       false ; unmatched terms
       env)))
+
+(defn match-star [sv env terms]
+  (loop [terms terms, matches []]
+    (if (seq terms)              
+      (if-let [env' (match env sv (first terms))]
+        (recur (rest terms) (conj matches (get env' sv)))
+        [matches terms])
+      [matches terms])))
+
+(defn count-min-expected-terms [patts]
+  (loop [patts patts, count 0]
+    (if (seq patts)
+      (if (or (star-variable? (first patts))
+              (opt-variable? (first patts)))
+        (recur (rest patts) count)
+        ;; otherwise count is incremented
+        (recur (rest patts) (inc count)))
+      ;; no more patterns
+      count)))
+
+(comment
+  (count-min-expected-terms '(?X toto ?Y* ?Z+ 42 ?Z?))
+)
  
 (comment     
   (match '?X '(==> A A))
@@ -61,6 +197,10 @@
   (match '(==> ?X ?Y) '(==> A A))
   (match '(==> ?X ?X) '(==> A A))
   (match '(==> ?X ?X) '(==> A B))
+
+  (match '(==> ?X*) '(==> A B C))
+  (match '(==> ?X* ?Y) '(==> A B C))
+
   )
 
 
@@ -126,6 +266,8 @@
 
 
 ;; (search-theorem 'latte-prelude.prop '(==> (and ?X ?Y) (and ?Y ?X)))
+;; (search-theorem 'latte-prelude.prop '(==> (==> ?X ?Y) ?Z ?T))
+;; (search-theorem 'latte-prelude.prop '(==> ?X (not ?X) ?Y))
 
 
 
